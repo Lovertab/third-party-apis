406da13552c4f7ca9191aff6efd84c1d2330b6e8
Lovertab <lovertabrown@gmail.com>
2024-03-29 09:10:47 -0400

    updating

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,20 +1,62 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+renderTaskList(false);
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-
+const addTask = document.getElementById("add-task");
+const submitTask = document.getElementById("submit-task");
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
 }
-
+// this shows the task-form
+addTask.addEventListener("click", function () {
+    document.getElementById("task-form").style.display = "block";
+});
+// this hids the task
+submitTask.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("task-form").style.display = "none";
+    renderTaskList(true);
+});
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+    const container = document.createElement("div")
+    const title = document.createElement("p")
+    title.innerText = task.title
+    container.appendChild(title)
+    const description = document.createElement("p")
+    description.innerText = task.description
+    container.appendChild(description)
+    const date = document.createElement("p")
+    date.innerText = task.date
+    container.appendChild(date)
+    const remove = document.createElement("button")
+    remove.innerText = "delete"
+    container.appendChild(remove)
+    return container;
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
+function renderTaskList(isNew) {
+    let taskList = JSON.parse(localStorage.getItem("tasks")) || []
+    if (isNew) {
+        let newTask = {
+        title: document.getElementById("title").value,
+        date: document.getElementById("date").value,
+        description: document.getElementById("description").value,
+        status:"to-do"
+    }
+    // push adds to array. append is adding to the html
+    taskList.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    }
+    for (let i = 0; i < taskList.length; i++){
+        const task = taskList[i];
+        const cardEl = createTaskCard(task)
+        if (task.status === "to-do") {
+            document.getElementById("todo-cards").appendChild(cardEl);
+            
+        }
+    }
 }
 
 // Todo: create a function to handle adding a new task
